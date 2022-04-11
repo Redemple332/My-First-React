77770db832c6b1b97b93ff4bad89e8f1ff521653
Redemple32 <marceloredemple332@gmail.com>
2022-04-11 08:57:13 +0800

    install tailwind

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Sample from "./components/Sample.js";
+function App() {
+  return <Sample />;
+}
+
+export default App;
---
+++ src/components/Sample.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Sample() {
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+}
